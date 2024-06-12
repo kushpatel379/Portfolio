9784a78e9b6b11591c96fd6664f19448b670134d
Kush <kushpatel379@gmail.com>
2024-06-12 12:48:02 -0400

    Added js file and made logos clickable

---
+++ script/portfolio.js
@@ -0,0 +1,7 @@
+document.getElementById("linkedin-logo").addEventListener('click', function(e){
+    window.location.href = 'https://www.linkedin.com/in/kush-p-68730a153/';
+})
+
+document.getElementById("github-logo").addEventListener('click', function(e){
+    window.location.href = 'https://github.com/kushpatel379';
+})
